9283f18cad78f95e41961075c7106ed058d4f3e0
AndreyLytvynov <mrjastonehome@gmail.com>
2023-05-18 16:37:49 +0300

    add toasts and add redirect by success submite form

--- src/components/FormSubscribe/FormSubscribe.tsx
+++ src/components/FormSubscribe/FormSubscribe.tsx
@@ -1,10 +1,14 @@
-import { FC } from "react";
+import { FC, useCallback, useEffect } from "react";
+import { useRouter } from "next/router";
+
 import { useForm, SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { schema } from "@/helpers/validateFormSchema";
 
 import SubmitFormBtn from "@/components/Buttons/SubmitFormBtn";
 
-import { schema } from "@/helpers/validateFormSchema";
+import { ToastMessageType } from "@/components/Toast/Toast";
+import ToastMessage from "@/components/Toast/Toast";
 
 export type IFormValues = {
   name: string;
@@ -22,10 +26,28 @@ const FormSubscribe: FC = () => {
   } = useForm<IFormValues>({
     resolver: yupResolver(schema),
   });
+  const router = useRouter();
 
-  const onSubmit: SubmitHandler<IFormValues> = (data) => {
+  const notify = useCallback((type: ToastMessageType, message: string) => {
+    ToastMessage({ type, message });
+  }, []);
+
+  useEffect(() => {
+    if (errors.name || errors.email || errors.phone) {
+      notify("error", "Please fill in all fields.");
+    }
+    if (errors.newsletter) {
+      notify("error", "Please confirm the email submission.");
+    }
+  }, [errors, notify]);
+
+  const onSubmit: SubmitHandler<IFormValues> = async (data) => {
     console.log(data);
+    notify("success", "Success! Please check your email.");
     reset();
+    setTimeout(() => {
+      router.replace("/");
+    }, 2000);
   };
 
   return (
--- src/pages/subscribe/index.tsx
+++ src/pages/subscribe/index.tsx
@@ -3,10 +3,23 @@ import { FC } from "react";
 import Container from "@/components/Container/Container";
 import SubscribeFormSection from "@/components/Sections/SubscribeFormSection";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const SubscribePage: FC = () => {
   return (
     <Container>
       <SubscribeFormSection />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        draggable={false}
+        closeOnClick
+        pauseOnHover
+        className="mb-20"
+      />
     </Container>
   );
 };
