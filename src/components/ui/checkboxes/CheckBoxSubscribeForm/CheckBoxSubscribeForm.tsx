535d839708231f1a91804620dfc95a417d871028
AndreyLytvynov <mrjastonehome@gmail.com>
2023-05-23 22:24:49 +0300

    fix checkbox error position

--- src/components/ui/checkboxes/CheckBoxSubscribeForm/CheckBoxSubscribeForm.tsx
+++ src/components/ui/checkboxes/CheckBoxSubscribeForm/CheckBoxSubscribeForm.tsx
@@ -9,7 +9,7 @@ const CheckBoxSubscribeForm: FC<CheckBoxFieldProps> = ({
   return (
     <label className="w-full sm:flex items-center mb-6" htmlFor={id}>
       <div className="sm:w-20"></div>
-      <div className="sm:w-96 flex text-gray-500 font-bold">
+      <div className="sm:w-96 flex text-gray-500 font-bold relative">
         <input
           className="mr-2 leading-tight"
           type="checkbox"
@@ -18,7 +18,7 @@ const CheckBoxSubscribeForm: FC<CheckBoxFieldProps> = ({
         />
         <span className="text-sm">Send me your newsletter!</span>
         {error && (
-          <div className="absolute text-red-500 text-xs font-normal">
+          <div className="absolute text-red-500 text-xs font-normal top-5">
             {error}
           </div>
         )}
